6e7916864304e7dfff03f4e116f12b0a16366d93
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-10-13 23:04:05 +0200

    début implémentation de la classe de gestion des screen reliés à splashActivity

--- app/src/main/java/com/example/myapplication2/SplashActivity.java
+++ app/src/main/java/com/example/myapplication2/SplashActivity.java
@@ -10,5 +10,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
     }
 }
