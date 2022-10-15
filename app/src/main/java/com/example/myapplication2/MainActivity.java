df51faf3c8f8b29b43943e9c494b04835504d1a7
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-10-15 19:24:01 +0200

    ajout variable encapsulation private pour le tasksRecyclerView, utilisation de .hide sur la support action bar qui est visible par defaut.

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide;
     }
 }
