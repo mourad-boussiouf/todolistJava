884aed650efea19eb5e37f08ffb75604383f70e0
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-10-15 22:30:23 +0200

    instanciation class LinearLayoutManager pour gérer le xml du recycler view de la liste des tâches

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -16,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide;
 
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
