a8666add957018ab31a7326a85f1814f47218385
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-10-17 00:29:29 +0200

    initilisation variable private MainActivity, initiation méthode ToDOAdapter public

--- app/src/main/java/com/example/myapplication2/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/myapplication2/Adapter/ToDoAdapter.java
@@ -2,7 +2,14 @@ package com.example.myapplication2.Adapter;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication2.MainActivity;
+
 public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     private List<ToDoModel> todoList;
+    private MainActivity activity;
+
+    public ToDoAdapter(MainActivity activity) {
+        this.activity = activity;
+    }
 }
