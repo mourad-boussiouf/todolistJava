9fc183d1d4e162653a0e99d0d094e6dc47f0ffb7
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-10-26 17:14:33 +0200

    classe de cration de taches enfant du fragment bas d'écran de base android dev, import widgt EditText de :android pour gérer la textArea de l'input, variable du contenu de la textarea String TAG définie.

--- app/src/main/java/com/example/myapplication2/AddNewTask.java
+++ app/src/main/java/com/example/myapplication2/AddNewTask.java
@@ -1,6 +1,12 @@
 package com.example.myapplication2;
 
+import android.widget.EditText;
+
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class AddNewTask extends BottomSheetDialogFragment {
+
+    public static final String TAG = "ActionBottomDialog";
+
+    private EditText newTaskText;
 }
