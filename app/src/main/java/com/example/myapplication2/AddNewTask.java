f6edb71aae286b78727350dd8ac8fd36f78949bc
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-10-26 17:36:14 +0200

    widget DatabaseHandler indispo création classe locale provisoisement pour gérer la bdd

--- app/src/main/java/com/example/myapplication2/AddNewTask.java
+++ app/src/main/java/com/example/myapplication2/AddNewTask.java
@@ -1,5 +1,6 @@
 package com.example.myapplication2;
 
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
@@ -9,4 +10,6 @@ public class AddNewTask extends BottomSheetDialogFragment {
     public static final String TAG = "ActionBottomDialog";
 
     private EditText newTaskText;
+    private Button newTaskSaveButton;
+    private DatabaseHandler db;
 }
