6ecc91aec9722013d604cce56647d9a699af2dc1
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-11-09 15:22:41 +0100

    concatenation requete SQL complete CREATE TODO TABLE

--- app/src/main/java/com/example/myapplication2/DatabaseHandler.java
+++ app/src/main/java/com/example/myapplication2/DatabaseHandler.java
@@ -1,5 +1,7 @@
 package com.example.myapplication2;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -8,5 +10,13 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String NAME = "toDoListDatabase";
     private static final String TODO_TABLE = "todo";
     private static final String ID = "id";
+    private static final String TASK = "task";
+    private static final String STATUS = "status";
+    private static final String CREATE_TODO_TABLE = "CREATE TABLE" + TODO_TABLE + "(" + ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                    + TASK + "TEXT," + STATUS + "INTEGER)";
+    private SQLiteDatabase db;
 
+    private DatabaseHandler(Context context){
+        super(context, NAME,null, VERSION);
+    }
 }
