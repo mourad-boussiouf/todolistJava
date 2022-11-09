5142355ec0bdb5b54586d061ec57c0ac8323e26e
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-11-09 14:36:20 +0100

    set variables DatabaseHandler

--- app/src/main/java/com/example/myapplication2/DatabaseHandler.java
+++ app/src/main/java/com/example/myapplication2/DatabaseHandler.java
@@ -4,4 +4,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
+    private static final int VERSION = 1;
+    private static final String NAME = "toDoListDatabase";
+    private static final String TODO_TABLE = "todo";
+    private static final String ID = "id";
+
 }
