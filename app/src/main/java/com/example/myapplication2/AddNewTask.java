59626facd6f053e5529c12dee9fd936b1b3b67f4
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-11-25 11:16:50 +0100

    création interface qui servira à la gestion des fenetres de dialog DialogCloseListener

--- app/src/main/java/com/example/myapplication2/AddNewTask.java
+++ app/src/main/java/com/example/myapplication2/AddNewTask.java
@@ -2,6 +2,9 @@ package com.example.myapplication2;
 
 import com.example.myapplication2.Model.ToDoModel;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -94,7 +97,15 @@ public class AddNewTask extends BottomSheetDialogFragment {
                     task.setTask(text);
                     task.setStatus(0);
                 }
+                dismiss();
              }
          });
+    }
+    @Override
+    public void onDismiss(DialogInterface dialog){
+        Activity activity = getActivity();
+        if(activity instanceof DialogCloseListener){
+            ((DialogCloseListener)activity).handleDialogClose(dialog);
+        }
     }
 }
