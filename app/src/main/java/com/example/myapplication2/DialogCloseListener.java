2ff81fd8ed400501e3f86f93961eb88047f65c24
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-11-25 11:31:59 +0100

    interface gestion fermeture pop up de dialogue terminée

--- app/src/main/java/com/example/myapplication2/DialogCloseListener.java
+++ app/src/main/java/com/example/myapplication2/DialogCloseListener.java
@@ -1,4 +1,8 @@
 package com.example.myapplication2;
 
+import android.content.DialogInterface;
+
 public interface DialogCloseListener {
+
+    public void handleDialogClose(DialogInterface dialog);
 }
